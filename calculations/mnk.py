2d3b7d992b33ab44db5b49009db83f53b13c92ec
Krivtsov Pavel <pkrivtsov6@gmail.com>
2021-06-05 10:40:54 +0300

    changed mean values calculation

--- calculations/mnk.py
+++ calculations/mnk.py
@@ -2,16 +2,8 @@ import typing as tp
 
 
 def find_mnk_odds(X: tp.List[float], Y: tp.List[float]) -> tp.Tuple[float, float]:
-    x_sum = 0.0
-    y_sum = 0.0
-
-    for x in X:
-        x_sum += x
-    for y in Y:
-        y_sum += y
-
-    X_mean = x_sum / len(X)
-    Y_mean = y_sum / len(Y)
+    X_mean = sum(X) / len(X)
+    Y_mean = sum(Y) / len(Y)
 
     numerator = 0.0
     denominator = 0.0
